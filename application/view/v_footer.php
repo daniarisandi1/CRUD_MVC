996a9000ec3f4dd8252946dd5c104b0b8fee7285
daniarisandi1 <44937442+daniarisandi1@users.noreply.github.com>
2018-11-12 05:04:16 +0700

    Add files via upload

---
+++ application/view/v_footer.php
@@ -0,0 +1,10 @@
+<br>
+    <footer class="panel-footer w3-light-grey">
+        <p class="copyright text-muted text-center">Copyright &copy;</p>
+    </footer>
+    <script type="text/javascript" src="<?php base_url('assets/js/jquery.js');?>"></script>
+    <script type="text/javascript" src="<?php base_url('assets/js/jquery.min.js');?>"></script>
+    <script type="text/javascript" src="<?php base_url('assets/js/bootstrap.js');?>"></script>
+    <script type="text/javascript" src="<?php base_url('assets/js/bootstrap.min.js');?>"></script>
+</body>
+</html>
